f0ae124489c65e1b8e3be5333f5880e80d8c8694
Stevenzhai <Stevenzwzhai@users.noreply.github.com>
2017-05-05 00:18:49 +0800

    fixed bug

--- mkdirs.js
+++ mkdirs.js
@@ -6,19 +6,28 @@ const fs = require('fs');
 // });
 
 function mkdirs(pathname, callback){
+	//获取创建路径
+	 let root = path.dirname(module.parent.filename);
 	//判断传入的是否是一个绝对路径
-	pathname = path.isAbsolute(pathname)?pathname:path.join(__dirname, pathname);
+	pathname = path.isAbsolute(pathname)?pathname:path.join(root, pathname);
 	//D:\webstorm\node1\demo2\demo3
+	
 	//获取创建部分
 	// pathname = pathname.replace(__dirname, '');
-	let relativepath = path.relative(__dirname, pathname);
+	let relativepath = path.relative(root, pathname);
 
 	let folders = relativepath.split(path.sep);
 	//这里是阻塞的
 	try{
 		let pre = '';
 		folders.forEach(folder => {
-			fs.mkdirSync(path.join(__dirname, pre, folder));
+			try{
+				fs.stat(path.join(root, pre, folder));
+				fs.mkdirSync(path.join(root, pre, folder));
+			}catch(error){
+			
+			}
+			
 			pre+=path.sep+folder;
 			// pre = path.join(pre, folder);
 		})	
